faf3ecd2f3afb5378658ba224f6f0dc32f2f869b
clanker <50203404+elkurto343@users.noreply.github.com>
2026-08-22 15:51:51 +1000

    Bump Traefik to v3.7.11
    3.6 reached EOL 2026-08-16 and won't get further patches. Fixes an
    unauthenticated critical auth bypass in the digestAuth middleware
    (GHSA-5w68-77r2-r64c, CVSS 9.3) that upstream only patched in 3.7.11,
    not backported to 3.6.
    
    SO-387

--- nix-dokploy.nix
+++ nix-dokploy.nix
@@ -210,7 +210,7 @@ in {
     traefik = {
       image = lib.mkOption {
         type = lib.types.str;
-        default = "traefik:v3.6.25";
+        default = "traefik:v3.7.11";
         description = ''
           Traefik Docker image to use.
         '';
